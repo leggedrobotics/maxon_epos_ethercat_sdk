5248570c9806139ffb27970bcf5be7903c2d2e4c
jungerj <jungerj.engineer@gmail.com>
2021-03-21 21:53:24 +0100

    operator<< for ModeOfOperationEnum

--- include/maxon_epos_ethercat_sdk/ModeOfOperationEnum.hpp
+++ include/maxon_epos_ethercat_sdk/ModeOfOperationEnum.hpp
@@ -20,6 +20,7 @@
 #pragma once
 
 #include <cstdint>
+#include <iostream>
 
 namespace maxon
 {
@@ -35,3 +36,5 @@ enum class ModeOfOperationEnum : int8_t
 };
 
 }  // namespace maxon
+
+std::ostream& operator << (std::ostream& os, const maxon::ModeOfOperationEnum modeOfOperation);
